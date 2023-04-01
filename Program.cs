655d813d89d028f32fb98831cb98377bd16356e8
Imjaopedro <125218310+Imjaopedro@users.noreply.github.com>
2023-04-01 17:34:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int maior = 0;
+            int menor = 0;
+            int i = 0;
+            
+            while (i <= 10){
+                Console.Write("Entre com um número: ");
+                num = int.Parse(Console.ReadLine());
+                i+=1;
+
+                if (maior < num){
+                    maior = num;
+                }
+                else {
+                    menor = num;
+                }
+                
+            }
+        Console.WriteLine($"O maior numero foi {maior} e o menor foi {menor}");
+        }
+    }
+}
